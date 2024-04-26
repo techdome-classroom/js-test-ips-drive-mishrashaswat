47af7ecbfcef9ab6e10cf65b14a419b06c6fe30c
mishrashaswat <168154420+mishrashaswat@users.noreply.github.com>
2024-04-26 13:28:13 +0530

    Update program1.js
    solution 1

--- program1.js
+++ program1.js
@@ -1,7 +1,30 @@
 function smallestMissingPositiveInteger(nums) {
 
 
-    // Implement the function smallest_missing_positive_integer
+    
+    nums-nums.filter (num=>num>0);
+    
+    if(nums.length==0) {
+      return 1;
+    }   
+    
+    nums.sort((a,b) =>a-b);
+    
+    let smallest_Missing=1;
+    
+    for(const num of nums) {
+    
+    if(num===smallest_Missing) {
+    
+    smallest_Missing++;
+    
+    }else{
+    
+    return smallest_Missing
+    }
+    return smallest_Missing;
+  }
+    
   
   }
   
