02c9b4d5351c8ea37b431006745a1dbdd99de2eb
mishrashaswat <168154420+mishrashaswat@users.noreply.github.com>
2024-04-26 13:36:00 +0530

    Update program2.js
    solution2

--- program2.js
+++ program2.js
@@ -1,7 +1,22 @@
 function longestSubstring(s) {
 
 
-    // Implementation of longestSubstring function
+   let n=s . length;
+    if(n===0)return 0;
+    let maxlength=0;
+    const seen ={};
+    let s=0;    
+    for(let  end=0;end<n;end++){
+        if(s[end]in seen&&s<=seen[s[end]]){
+            s=seen[s[end]]+1
+        } else {
+            maxlength=Math.max(maxlength,end-s+1);
+
+        }
+        seen[s[end]]=end
+    }
+    return maxlength;
+
 }
 
 module.exports = { longestSubstring };
